98a224cc1a9aaeec5abaa460f429dbdaf7bb5d42
joban <jobansingh03@gmail.com>
2018-12-14 16:35:29 -0500

    level changes added

---
+++ Assets/HealtBar.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealtBar : MonoBehaviour
+{
+    public List<GameObject> hearts;
+    public GameObject player;
+    void playGameOver()
+    {
+        if (hearts.Count == 0)
+        {
+            Destroy(this.player);
+        }
+        else { }
+    }
+}
---
+++ Assets/nextLevel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class nextLevel : MonoBehaviour
+{
+
+    public GameObject player;
+   
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+
