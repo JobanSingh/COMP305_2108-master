1753ef012e32c1d6de4d424bb4ddb3bdd4ba2102
joban <jobansingh03@gmail.com>
2018-12-14 17:07:13 -0500

    sprites

--- Assets/nextLevel.cs
+++ Assets/nextLevel.cs
@@ -10,6 +10,7 @@ public class nextLevel : MonoBehaviour
     void OnTriggerEnter2D(Collider2D other)
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        
         }
     }
 
